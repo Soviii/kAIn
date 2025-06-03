a32abe78ae2053abdfa36d2c2adce1275e863bf3
Sovial Sonzeu <ssonzeu@gmail.com>
2025-06-02 21:35:05 -0700

    starting docker container files and setup - frontend, backend, and db containers

---
+++ backend/src/main/java/com/example/HelloController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/goodbye")
+    public Map<String, String> sayGoodbye() {
+        return Map.of("message", "goodbye planet");
+    }
+}
+
