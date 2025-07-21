6d1c7d44b8d338aa5f244e83e18ac57c63c83b1f
Sophia Canja <sophia.canja1@gmail.com>
2025-07-20 20:33:26 -0700

    #10 Created the ingredient model

---
+++ backend/src/main/java/com/example/ingredients/model/Ingredient.java
@@ -0,0 +1,72 @@
+package com.example.ingredients.model;
+
+import com.example.recipes.model.Recipe;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Maps to SERIAL in PostgreSQL (auto-increment)
+    private Long id;
+
+    private String name;
+    private double quantity;
+    private String unit;
+
+    // creating a foreign key relationship with the Recipe entity
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_uuid", nullable = false)
+    private Recipe recipe;
+
+    // Constructors, getters, and setters
+    public Ingredient() {}
+
+    public Ingredient(String name, double quantity, String unit, Recipe recipe) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(double quantity){
+        this.quantity = quantity;
+    }
+
+    public String getUnit(){
+        return unit;
+    }
+
+    public void setUnit(String unit){
+        this.unit = unit;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe= recipe;
+    }
+}
