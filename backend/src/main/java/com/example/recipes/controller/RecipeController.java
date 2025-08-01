0671ad07d704c5d7f9d2f51f70617e6527b0ef1f
Sophia Canja <sophia.canja1@gmail.com>
2025-07-31 20:27:52 -0700

    #10 updated RecipeController with documentation

--- backend/src/main/java/com/example/recipes/controller/RecipeController.java
+++ backend/src/main/java/com/example/recipes/controller/RecipeController.java
@@ -1,16 +1,13 @@
 package com.example.recipes.controller;
 
 import java.net.URI;
-import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.recipes.dto.RecipeRequestDTO;
 import com.example.recipes.dto.RecipeResponseDTO;
-import com.example.recipes.model.Recipe;
 import com.example.recipes.service.RecipeService;
 
 
@@ -24,41 +21,22 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    // TODO uncomment when ready to implement functions
-    // public RecipeResponseDTO getRecipe(Long id) {
-    //     return recipeService.getRecipeById(id);
-    // }
-
-    // TODO uncomment when ready to implement functions
-    // public List<RecipeResponseDTO> getAllRecipes() {
-    //     return recipeService.getAllRecipes();
-    // }
-
-    // @PostMapping
-    // public RecipeResponseDTO createRecipe(RecipeRequestDTO recipeRequestDTO) {
-    //     return recipeService.createRecipe(recipeRequestDTO);
-    // }
     @PostMapping
     public ResponseEntity<RecipeResponseDTO> createRecipe(@RequestBody RecipeRequestDTO recipeRequestDTO) {
         RecipeResponseDTO savedRecipe = recipeService.createRecipe(recipeRequestDTO);
+
+        // 201 Created response with Location header pointing to the new resource
+        // ServletUriComponentsBuilder helps build the URI for the newly created resource
         URI location = ServletUriComponentsBuilder
             .fromCurrentRequest()             // http://localhost:8080/recipes
             .path("/{id}")
             .buildAndExpand(savedRecipe.getRecipeId())
             .toUri();
 
+        // Return ResponseEntity with status 201 Created and the location of the new resource
         return ResponseEntity
             .created(location)                // 201 Created + Location: /recipes/{uuid}
-            .body(savedRecipe);                   // JSON body = your RecipeResponseDTO
+            .body(savedRecipe);               // JSON body = your RecipeResponseDTO
     
     }
-
-    // TODO uncomment when ready to implement functions
-    // public RecipeResponseDTO updateRecipe(Long id, RecipeRequestDTO recipeRequest) {
-    //     return recipeService.updateRecipe(id, recipeRequest);
-    // }
-
-    // public void deleteRecipe(Long id) {
-    //     recipeService.deleteRecipe(id);
-    // }
 }
