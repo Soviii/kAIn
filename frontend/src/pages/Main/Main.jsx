6f5d7ac3b530679148d8bb0399585198c3e41981
Sovial Sonzeu <ssonzeu@gmail.com>
2025-08-10 03:50:55 -0700

    created RecipeDetails and TabSelection components - different views based on current tab selected - updated Main.jsx with new components for different views

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -2,21 +2,38 @@ import React, { useState } from 'react';
 import RecipeGallery from '../../components/RecipeGallery/RecipeGallery';
 import ChatPane from '../../components/ChatPane/ChatPane';
 import TopNavBar from '../../components/TopNavbar/TopNavbar.jsx';
+import TabSelection from '../../components/TabSelection/TabSelection.jsx';
+import RecipeDetails from '../../components/RecipeDetails/RecipeDetails.jsx';
 import './Main.css';
 
 const Main = () => {
+  const [currView, setCurrView] = useState(0); // 0 for recipes, 1 for recipe details and AI chat
+
+  // receives updates from TabSelection component to render new view
+  const updateMainPage = (newView) => {
+    setCurrView(newView);
+  }
 
   return (
     <div className="container-fluid">
       <TopNavBar />
-        <div className="row min-vh-100">
-          <div className="col-md-6 d-flex flex-column p-4 border-end bg-white">
-            <RecipeGallery />
-          </div>
-          <div className="col-md-6 d-flex flex-column p-4 bg-light">
-            <ChatPane />
+      <TabSelection updateMainPage={updateMainPage} />
+      {/* changes view based on TabSelection */}
+      {currView === 0 ? (
+        <RecipeGallery />
+      ) : (
+        <div className="container-fluid">
+          <div className="row min-vh-100">
+            <div className="col-12 col-md-6 d-flex flex-column p-4 border-end bg-white">
+              <RecipeDetails />
+            </div>
+            <div className="col-12 col-md-6 d-flex flex-column p-4 bg-light">
+              <ChatPane />
+            </div>
           </div>
         </div>
+      )}
+
     </div>
   );
 };
