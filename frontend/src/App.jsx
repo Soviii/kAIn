3d2ad3ee7a56ad7c56807f99be41c4c4b53cd04a
Sovial Sonzeu <ssonzeu@gmail.com>
2025-08-11 00:37:25 -0700

    added basic router for navigating to different pages using top navbar #24 - added different pages - top navbar currently shows logged in and logged out states (TODO: will update once we have JWT's in place) - updated App.jsx and index.js to have correct purpose and components - removed Body.jsx and Nav.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,96 @@
-
-import './App.css';
-import Nav from './components/Nav';
-import Body from './components/Body';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutUs from './pages/AboutUs/AboutUs.jsx';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword.jsx';
+import LoginAndRegister from './pages/LoginAndRegister/LoginAndRegister.jsx';
+import Main from './pages/Main/Main.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
+import Profile from './pages/Profile/Profile.jsx';
+import ResetPassword from './pages/ResetPassword/ResetPassword.jsx';
+import TopNavbar from './components/TopNavbar/TopNavbar.jsx';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: (
+        <>
+          <TopNavbar />
+          <LoginAndRegister />
+        </>
+      )
+    },
+    {
+      path: "/forgot-password",
+      element: (
+        <>
+          <TopNavbar />
+          <ForgotPassword />
+        </>
+      )
+    },
+    {
+      path: "/home",
+      element: (
+        <>
+          <TopNavbar />
+          <Main />
+        </>
+      )
+    },
+    {
+      path: "/not-found",
+      element: (
+        <>
+          <TopNavbar />
+          <NotFound />
+        </>
+      )
+    },
+    {
+      path: "/profile",
+      element: (
+        <>
+          <TopNavbar />
+          <Profile />
+        </>
+      )
+    },
+    {
+      path: "/reset-password",
+      element: (
+        <>
+          <TopNavbar />
+          <ResetPassword />
+        </>
+      )
+    },
+    {
+      path: "/about-us",
+      element: (
+        <>
+          <TopNavbar />
+          <AboutUs />
+        </>
+      )
+    },
+    {
+      path: "/*",
+      element: (
+        <>
+          <TopNavbar />
+          <NotFound />
+        </>
+      )
+    }
+  ])
 
   return (
-    <>
-      <div>
-       
-        <Nav />
-        <Body />
-      </div>
-    </>
-  );
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  )
 }
 
 export default App;
--- frontend/src/components/TopNavbar/TopNavbar.jsx
+++ frontend/src/components/TopNavbar/TopNavbar.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import kainLogo from "../../assets/kAIn-logo-v2.jpeg";
 import "./TopNavbar.css";
 
 
 // TODO: update img with sprite of luffy or continue using current logo
-  // may need to color match background of jpg/png with color for navbar...
+// may need to color match background of jpg/png with color for navbar...
 const TopNavbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // signs the user out 
+    // TODO: handle JWT as well... maybe remove ???
+  const handleSignOut = () => {
+    setIsLoggedIn(!isLoggedIn);
+  }
+
+  // checks if current JWT is still valid
+  // TODO: implement JWT and check JWT ...
+  const checkIfLoggedIn = () => {
+    setIsLoggedIn(true); // temporary; for testing
+    return;
+  }
+
+  // for checking if logged in - renders different options based on logged in status
+  useEffect(() => {
+    checkIfLoggedIn();
+  }, [])
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand className="fw-bold" href="/main">
+        <Navbar.Brand className="fw-bold" href="/home">
           <img src={kainLogo}
             className="logo-img"
             width="30"
@@ -21,10 +41,19 @@ const TopNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto"> {/* ms-auto (margin start auto) allows nav links align right while brand stays on the left */}
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about-us">About Us</Nav.Link>
-            <Nav.Link href="#profile">Profile</Nav.Link>
-            <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+            {isLoggedIn === true ?
+              <>
+                <Nav.Link href="/about-us">About Us</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
+              </>
+              :
+              <>
+                <Nav.Link href="/about-us">About Us</Nav.Link>
+                <Nav.Link href="/login">Login/Register</Nav.Link>
+                <Nav.Link href="/reset-password">Reset Password</Nav.Link>
+              </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Container>
